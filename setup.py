dbf9850d0c5f149acdbd438424e744666e161fc3
Louis Person <louis.person@epitech.eu>
2015-04-16 11:36:54 +0200

    Bundle setuptools

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
+import ez_setup
+ez_setup.use_setuptools()
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='airbag',
 	  version='0.1.0',
@@ -9,7 +11,7 @@ setup(name='airbag',
 	  author_email='lait.kureuil@gmail.com',
 	  url='https://github.com/kureuil/airbag',
 	  install_requires=['pytoml'],
-	  packages=['airbag'],
+	  packages=find_packages(),
 	  entry_points='''
 		[console_scripts]
 		testrunner=airbag.application:cli
