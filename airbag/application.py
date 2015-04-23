6a9258655f52bbd3bbd1aaf864c34fa0d09e8ed0
Louis Person <louis.person@epitech.eu>
2015-04-23 14:22:13 +0200

    Implemented CLI options for config file path & working directory

--- airbag/application.py
+++ airbag/application.py
@@ -4,18 +4,45 @@ if (version_info < (3, 3, 0)):
 	stderr.write('Python 3.3 is required to run this program')
 	exit(1)
 
+import argparse
+from os import getcwd, chdir
 from .testrunner import TestRunner
 from .config.toml import TomlConfig
 
 def cli():
+	parser = argparse.ArgumentParser(
+		description='Runs functional tests on your programs'
+	)
+	parser.add_argument(
+		'-d', '--working-dir',
+		type=str,
+		metavar='DIR',
+		default=getcwd(),
+		help='Changes the working directory'
+	)
+	parser.add_argument(
+		'-f', '--input-file',
+		type=str,
+		metavar='FILE',
+		default='airbag.toml',
+		help='Changes the configuration used'
+	)
+	parser.add_argument(
+		'-V', '--version',
+		action='version',
+		version='Airbag 0.2',
+		help='Displays the current program\'s version and exit'
+	)
+	args = parser.parse_args()
 	try:
-		config = TomlConfig("airbag.toml")
+		config = TomlConfig(args.input_file)
 	except ValueError:
 		stderr.write('Error during configuration file parsing.\n')
 		exit(1)
 	except FileNotFoundError:
 		exit(1)
 	else:
+		chdir(args.working_dir)
 		runner = TestRunner(config)
 		if runner.launch() != 0:
 			exit(1)
